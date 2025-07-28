8f330ff7f98b6dfa760a19ae0ada81468147a374
Nithin Kumar Gorintala <121857100+nithintechie123@users.noreply.github.com>
2025-07-28 13:09:41 +0530

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import HeroSection from '../components/HeroSection'
 import FeaturedSection from '../components/FeaturedSection'
 import TrailersSection from '../components/TrailersSection'
 
+
 const Home = () => {
   return (
     <>
