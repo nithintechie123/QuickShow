b4aec06c50397345824ea422ef8f22d5193141fe
Nithin Kumar Gorintala <121857100+nithintechie123@users.noreply.github.com>
2025-07-31 18:41:03 +0530

    4th commit

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -6,6 +6,7 @@ import {Heart, PlayCircle, PlayCircleIcon, StarIcon} from "lucide-react"
 import timeFormat from '../lib/timeFormat';
 import DateSelect from '../components/DateSelect';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 const MovieDetails = () => {
   const navigate=useNavigate()
@@ -14,10 +15,13 @@ const MovieDetails = () => {
 
   const getShow=async ()=>{
       const show=dummyShowsData.find(show=>show._id==id)
-      setShow({
-        movie:show,
-        dateTime:dummyDateTimeData
-      })
+      if(show){
+        setShow({
+          movie:show,
+          dateTime:dummyDateTimeData
+        })
+      }
+      
   }
 
   useEffect(()=>{
@@ -87,7 +91,7 @@ const MovieDetails = () => {
         transition rounded-md font-medium cursor-pointer'>Show more</button>
       </div>
     </div>
-  ):<div>Loading...</div>
+  ):<Loading/>
 }
 
 export default MovieDetails
