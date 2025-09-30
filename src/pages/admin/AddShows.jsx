1de9737a5f7b46a17fa55a2b45f25d3fdedd6b1f
Nithin Kumar Gorintala <121857100+nithintechie123@users.noreply.github.com>
2025-09-30 13:37:56 +0530

    Added backend server

--- src/pages/admin/AddShows.jsx
+++ src/pages/admin/AddShows.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { dummyShowsData } from '../../assets/assets';
 import Loading from '../../components/Loading';
 import Title from '../../components/admin/Title';
-import { CheckIcon, StarIcon } from 'lucide-react';
+import { CheckIcon, DeleteIcon, StarIcon } from 'lucide-react';
 import { kConverter } from '../../lib/kConverter';
 
 const AddShows = () => {
@@ -14,7 +14,7 @@ const AddShows = () => {
   const [dateTimeInput,setDateTimeInput]=useState("");
   const [showPrice,setShowPrice]=useState("");
   
-  const fetNowPlayingMovies=async()=>{
+  const fetchNowPlayingMovies=async()=>{
     setNowPlayingMovies(dummyShowsData)
   }
 
@@ -26,13 +26,28 @@ const AddShows = () => {
     setDateTimeSelection((prev)=>{
       const times=prev[date] || [];
       if(!times.includes(time)){
-        return {...prev,[date]:[...times,time]};
+        return {...prev,[date]:[...times,time]};  
+      }
+      return prev;
+    })
+  }
+
+  const handleRemoveTime=(date,time)=>{
+    setDateTimeSelection((prev)=>{
+      const filteredTimes=prev[date].filter((t)=>t!==time);
+      if(filteredTimes.length===0){
+        const {[date]:_,...rest}=prev;
+        return rest;
+      }
+      return {
+        ...prev,
+        [date]:filteredTimes,
       }
     })
   }
 
   useEffect(()=>{
-    fetNowPlayingMovies();
+    fetchNowPlayingMovies();
   },[])
 
   return ( nowPlayingMovies.length > 0 ? (
@@ -80,6 +95,7 @@ const AddShows = () => {
           </div>
         </div>
         {/* Date & Time Selection */}
+        <div className='mt-6'>
         <label className='block text-sm font-medium mb-2'>Select Date and Time</label>
         <div className="inline-flex gap-5 border border-gray-600 p-1 pl-3
         rounded-lg">
@@ -89,7 +105,36 @@ const AddShows = () => {
           text-white px-3 py-2 text-sm rounded-lg hover:bg-primary cursor-pointer">
             Add Time
           </button>
+          </div>
         </div>
+        {/* Selected Date & Time Display */}
+        {Object.keys(dateTimeSelection).length> 0 && (
+          <div className="mt-6">
+            <h2 className='mb-2'>Selected Date-Time</h2>
+            <ul className='space-y-3'>
+              {Object.entries(dateTimeSelection).map(([date,times])=>(
+                <li key={date}>
+                  <div className="font-medium">{date}</div>
+                  <div className="flex flex-wrap gap-2 mt-1 text-sm">
+                    {times.map((time)=>(
+                      <div key={time} className="border border-primary
+                      px-2 py-1 flex items-center rounded">
+                        <span>{time}</span>
+                        <DeleteIcon onClick={()=>handleRemoveTime(date,time)} width={15}
+                          className='ml-2 text-red-500 hover:text-red-700 
+                          cursor-pointer'/>
+                      </div>
+                    ))}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <button className="bg-primary text-white px-8 py-2 mt-6 rounded
+        hover:bg-primary/90 transition-all cursor-pointer">
+          Add Show
+        </button>
     </>
     ):<Loading/>
   )
